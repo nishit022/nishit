b32278dbfa7f76cfb71438500ab747e0c014d745
nishit022 <nishitg30@gmail.com>
2023-08-14 12:26:37 +0530

    new file install

---
+++ javascript/alert.js
@@ -0,0 +1,19 @@
+
+// alert("Hello...");
+
+// alert(1+2+3);
+
+
+            // document.getElementById("demo").innerHTML = "Nishit.";
+             function nishit(){
+                document.getElementById("demo").innerHTML="Nishit Goswami";
+             }
+       
+function sum(){
+    document.getElementById("demo").innerHTML=11+6 ;
+}
+function change(){
+    let a = "nishit1" ;
+            document.getElementById("demo4").innerHTML=  "the value of a is " + a + "." ;
+
+}
